9c4acc3c61e3c588be7b07e78990f285f1460f22
Erika Zibelnik <e.m.zibelnik@gmail.com>
2020-11-10 16:17:09 -0500

    adding contact page with css

--- reactportfolio/src/components/Contact/contact.js
+++ reactportfolio/src/components/Contact/contact.js
@@ -3,6 +3,19 @@ import "./style.css";
 
 function Contact(props) {
     return (
+        <form className="form">
+            <h1>Contact form phone</h1>
 
+            <label>Name</label>
+            <input placeholder="Name" />
+
+            <label>Email</label>
+            <input placeholder="Email" />
+
+            <label>Message</label>
+            <textarea placeholder="Message"></textarea>
+        </form>
     )
-}
+};
+
+export default Contact;
