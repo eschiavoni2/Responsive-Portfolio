ac16337917ac3ae31a02b6823d31bb4ae91ae75a
Erika Zibelnik <e.m.zibelnik@gmail.com>
2020-11-10 16:01:35 -0500

    added folders in components, made aboutme.js jsx

---
+++ reactportfolio/src/components/AboutMe/aboutme.js
@@ -0,0 +1,36 @@
+import React from "react";
+import "./style.css";
+
+function AboutMe(props) {
+    return (
+        <div class="container">
+        <div class="row">
+            <div class="col-md-12" id="about">
+                <h1>About Me</h1>
+                <hr />
+                <img class="main-img" src="profile.jpg" alt="Photo of Erika" />
+                <p class="font" style="font-size:18px">My name is Erika Zibelnik and I currently reside in Melbourne, Florida. I am learning
+                    full stack web development through UCF's
+                    coding
+                    bootcamp. This will prepare me for an environment that is looking for innovation, education, and
+                    creativity.</p>
+                <p style="font-size:18px">I have a diverse background which enables me to adapt and problem solve as needed. This is
+                    quite important in web development, and I believe will enable me to thrive. My past experiences have
+                    also had me work with diverse group of individuals, enabling me to work well with clients and
+                    co-operate
+                    with coworkers for the best results and productivity.
+                </p>
+                <p>
+                <a id="email" href="mailto:e.m.zibelnik@gmail.com" style="font-size:18px">Email me at e.m.zibelnik@gmail.com</a>
+                <br />
+                <a id="phone" href="tel:4403829494" style="font-size:18px">Call me at (440) 382-9494</a>
+                <div>
+                </div>
+                </p>
+            </div>
+        </div>
+    </div>
+    );
+}
+
+export default AboutMe;
