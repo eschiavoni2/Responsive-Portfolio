eb79b1f86fa9c2d093d2610614ccb53b810552d8
Erika Zibelnik <e.m.zibelnik@gmail.com>
2020-11-11 16:18:37 -0500

    working on router

--- reactportfolio/src/App.js
+++ reactportfolio/src/App.js
@@ -1,9 +1,10 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
+import { Route, BrowserRouter as Router } from "react-router-dom";
 import './App.css';
 import Contact from "./components/Contact/contact";
 import "./components/Contact/contact";
-import Navbar from "./components/Navbar/navbar";
-import Footer from "./components/Footer/footer";
+// import Navbar from "./components/Navbar/navbar";
+// import Footer from "./components/Footer/footer";
 import AboutMe from "./components/AboutMe/aboutme";
 import Portfolio from "./components/Portfolio/portfolio";
 
@@ -11,8 +12,13 @@ import Portfolio from "./components/Portfolio/portfolio";
 function App() {
   return (
     <Router>
-      <Navbar />
-      <Contact />
+      {/* <Navbar />
+      <Contact /> */}
+      <Route path="/" exact component={AboutMe} />
+      <Route path="/Contact" component={Contact} />
+      <Route path="/Portfolio" component={Portfolio} />
+
+
     </Router>
   );
 }
