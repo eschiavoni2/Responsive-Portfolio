f8fa10f0c00012ab1cd71fc98990dec8358f8cd3
Erika Zibelnik <e.m.zibelnik@gmail.com>
2020-11-12 11:32:49 -0500

    sorting out links, working on resume, and css

--- reactportfolio/src/App.js
+++ reactportfolio/src/App.js
@@ -1,46 +1,34 @@
-// import logo from './logo.svg';
-import { Route, BrowserRouter as Router, Switch, Link } from "react-router-dom";
-import './App.css';
-import Contact from "./components/Contact/contact";
-import "./components/Contact/contact";
-import Navbar from "./components/Navbar/navbar";
-import Footer from "./components/Footer/footer";
-import AboutMe from "./components/AboutMe/aboutme";
-import Portfolio from "./components/Portfolio/portfolio";
-
-
-function App() {
-  return (
-    <Router>
-      <div className="container">
-        <Navbar />
-        <nav>
-          <ul>
-            <li>
-              <Link to="/">About Me</Link>
-            </li>
-            <li>
-              <Link to="/Contact">Contact</Link>
-            </li>
-            <li>
-              <Link to="/Portfolio">Portfolio</Link>
-            </li>
-            {/* <li>
-              <Link to="/Resume">Resume</Link>
-            </li> */}
-          </ul>
-        </nav>
-      </div>
-      <Switch>
-        <Route path="/" exact component={AboutMe} />
-        <Route path="/Contact" component={Contact} />
-        <Route path="/Portfolio" component={Portfolio} />
-        {/* work on adding resume */}
-        {/* <Route */}
-      </Switch>
-      <Footer />
-    </Router>
-  );
+// import logo from './logo.svg';
+import { Route, BrowserRouter as Router, Switch, Link } from "react-router-dom";
+import './App.css';
+import Contact from "./components/Contact/contact";
+import "./components/Contact/contact";
+import Navbar from "./components/Navbar/navbar";
+import Footer from "./components/Footer/footer";
+import AboutMe from "./components/AboutMe/aboutme";
+import Portfolio from "./components/Portfolio/portfolio";
+function App() {
+  return (
+    <Router>
+        <Navbar />
+      <Switch>
+        <Route exact path="/">
+        <AboutMe />
+        </Route>
+        <Route exact path="/Contact">
+        <Contact />
+        </Route>
+        <Route path="/Portfolio">
+        <Portfolio />
+        </Route>
+        <Route path="/Resume">
+        <Portfolio />
+        </Route>
+        {/* work on adding resume */}
+        {/* <Route */}
+      </Switch>
+      <Footer />
+    </Router>
+  );
 }
-
-export default App;
+export default App;
--- reactportfolio/src/components/navbar/navbar.js
+++ reactportfolio/src/components/navbar/navbar.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import "./style.css";
+import { Link } from "react-router-dom";
 
 function Navbar() {
     return (
         <div className="outer-container">
-            <nav className="navbar navbar-expand-lg navbar-light bg-light" className="nav-container">
+            <nav className="navbar navbar-expand-lg navbar-light bg-light">
                 <a className="navbar-brand" id="name">Erika Zibelnik</a>
                 <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav"
                     aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
@@ -13,16 +14,16 @@ function Navbar() {
                 <div className="collapse navbar-collapse justify-content-end" id="navbarNav">
                     <ul className="navbar-nav">
                         <li className="nav-item active">
-                            <a className="nav-link" href="index.html">About Me<span className="sr-only">(current)</span></a>
+                            <Link to="/">About Me</Link>
                         </li>
                         <li className="nav-item">
-                            <a className="nav-link" href="portfolio.html">Portfolio</a>
+                            <Link to="/Portfolio">Portfolio</Link>
                         </li>
                         <li className="nav-item">
-                            <a className="nav-link" href="contact.html">Contact</a>
+                            <Link to="/Contact">Contact</Link>
                         </li>
                         <li className="nav-item">
-                            <a className="nav-link" href="Resume_ErikaZibelnik.pdf" target="_blank">Resume</a>
+                            <Link href="/Resume_ErikaZibelnik.pdf">Resume</Link>
                         </li>
                     </ul>
                 </div>
